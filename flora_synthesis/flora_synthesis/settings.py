f9c1a96a307b222372fb8dc34846bf0e41d6d9dc
etkewa@gmail.com <qsRf9sGKy9rV>
2023-09-07 15:51:46 -0700

    vercel

--- flora_synthesis/flora_synthesis/settings.py
+++ flora_synthesis/flora_synthesis/settings.py
@@ -79,12 +79,15 @@ WSGI_APPLICATION = 'flora_synthesis.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR/'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR/'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
