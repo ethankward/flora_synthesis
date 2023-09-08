a3cebef827912a43579fb030b9c55a548935fa88
etkewa@gmail.com <qsRf9sGKy9rV>
2023-09-07 19:03:43 -0700

    test

--- flora_synthesis/flora/views.py
+++ flora_synthesis/flora/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
+import os
 
 
 def test(request):
+    os.system("python manage.py migrate")
+
     html = "<html><body>test</body></html>"
     return HttpResponse(html)
