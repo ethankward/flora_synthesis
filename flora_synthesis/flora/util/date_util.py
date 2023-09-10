a3b9337a6ce46a0aea55e4f705beb61f7ec53111
etkewa@gmail.com <qsRf9sGKy9rV>
2023-09-09 23:14:47 -0700

    keep track of last date

--- flora_synthesis/flora/util/date_util.py
+++ flora_synthesis/flora/util/date_util.py
@@ -34,7 +34,8 @@ def combine_date_ranges(dates) -> typing.Generator[dict, None, None]:
                                 timezone.datetime(year=year, month=12, day=31).date())]):
             created_d1 = "{}-01-01".format(year)
             created_d2 = "{}-12-31".format(year)
-            yield {'created_d1': created_d1, 'created_d2': created_d2}
+            end_date = timezone.datetime(year=year, month=12, day=31)
+            yield {'created_d1': created_d1, 'created_d2': created_d2}, end_date
             full_years.add(year)
 
     full_months = set([])
@@ -48,10 +49,12 @@ def combine_date_ranges(dates) -> typing.Generator[dict, None, None]:
                 if (date.year, date.month) not in full_months:
                     created_d1 = "{}-{}-01".format(date.year, date.month)
                     created_d2 = "{}-{}-{}".format(date.year, date.month, last_month_day.day)
-                    yield {'created_d1': created_d1, 'created_d2': created_d2}
+                    end_date = timezone.datetime(year=date.year, month=date.month, day=last_month_day.day)
+                    yield {'created_d1': created_d1, 'created_d2': created_d2}, end_date
                     full_months.add((date.year, date.month))
 
     for date in dates:
         if date.year not in full_years:
             if (date.year, date.month) not in full_months:
-                yield {'created_on': "{}-{}-{}".format(date.year, date.month, date.day)}
+                end_date = timezone.datetime(year=date.year, month=date.month, day=date.day)
+                yield {'created_on': "{}-{}-{}".format(date.year, date.month, date.day)}, end_date
