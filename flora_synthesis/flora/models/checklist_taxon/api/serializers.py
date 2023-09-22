8c81e31f8a14eea5c3aa7e6b3799e9232f6a95f0
etkewa@gmail.com <qsRf9sGKy9rV>
2023-09-21 23:52:48 -0700

    serialize checklists consistently

--- flora_synthesis/flora/models/checklist_taxon/api/serializers.py
+++ flora_synthesis/flora/models/checklist_taxon/api/serializers.py
@@ -19,10 +19,11 @@ class ChecklistTaxonSerializer(serializers.ModelSerializer):
     family = serializers.SerializerMethodField()
     all_mapped_taxa = TaxonNameSerializer(many=True)
     primary_checklist = serializers.SerializerMethodField()
+    checklists = serializers.SerializerMethodField();
 
     class Meta:
         model = models.ChecklistTaxon
-        fields = ['id', 'taxon_name', 'family', 'external_id', 'rank', 'genus', 'checklist', 'all_mapped_taxa',
+        fields = ['id', 'taxon_name', 'family', 'external_id', 'rank', 'genus', 'checklists', 'all_mapped_taxa',
                   'primary_checklist']
 
     def get_family(self, obj):
@@ -30,3 +31,6 @@ class ChecklistTaxonSerializer(serializers.ModelSerializer):
 
     def get_primary_checklist(self, obj):
         return obj.checklist.primary_checklist
+
+    def get_checklists(self, obj):
+        return [obj.checklist.id]
